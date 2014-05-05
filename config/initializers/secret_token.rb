12c4273192d540e32ef9729c4c90d8a28d2e7452
Clare Vivas Lucas <clarevivaslucas@Clares-MacBook-Pro.local>
2014-05-05 10:01:01 -0400

    added all recipe_box files to local Git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeBox::Application.config.secret_key_base = '38735a8dea6e7b980a080a5f77d4922e64636b5319b91729395d7ab20ea67e09722b72cb51dd434db7de51872d7c4c7ed51f49f214c6b664e008f286a92eb2ec'
